8852fad61bed9a73632923d6af6c2eddf0bcf18c
Donald Chan <you@example.com>
2014-03-06 15:16:18 +1100

    Added delay to setup next photo

---
+++ g015v1/IEDGolfRoundViewController.m
@@ -0,0 +1,38 @@
+//
+//  IEDGolfRoundViewController.m
+//  g015v1
+//
+//  Created by Donald Chan on 6/03/2014.
+//  Copyright (c) 2014 iEndeavour. All rights reserved.
+//
+
+#import "IEDGolfRoundViewController.h"
+
+@interface IEDGolfRoundViewController ()
+
+@end
+
+@implementation IEDGolfRoundViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
